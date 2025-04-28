90f0efa586e86316f42ae241177c7d243b635cc2
Raymond londoño <raymond_5400@hotmail.com>
2025-04-27 22:49:23 -0500

    Add project files.

---
+++ struct3/Materia.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef MATERIA_H
+#define MATERIA_H
+
+namespace materia
+{
+	struct Materia
+	{
+		double* m_matematicas{ nullptr };
+		
+		double* m_sociales{ nullptr };
+
+		double* m_espanhol{ nullptr };
+
+		int m_capa_v_ma{};
+
+		int m_capa_v_so{};
+
+		int m_capa_v_es{};
+
+		//Funcionalidades del struct 
+
+
+
+	};
+	
+}
+
+#endif // !MATERIA_H
+
---
+++ struct3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	std::cout << "hola";
+
+	return 0;
+}
